72501d2e53804ee58d00a247dc506404a2dd76ee
Ronen Hilewicz <ronen@aserto.com>
2021-12-15 15:05:30 -0500

    A bunch of stuff

--- golang/install.fish
+++ golang/install.fish
@@ -1,6 +1,7 @@
 #!/usr/bin/env fish
-if status is-interactive
-    if command -qs gvm
-        echo "gvm use go1.17.4" > ~/.config/fish/conf.d/golang.fish
-    end
+if command -qs gvm
+    echo "gvm use go1.17.4" > ~/.config/fish/conf.d/golang.fish
+    echo "go env -w GOPRIVATE=github.com/aserto-dev" >> ~/.config/fish/conf.d/golang.fish
 end
+
+
